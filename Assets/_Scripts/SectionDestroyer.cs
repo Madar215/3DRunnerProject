05184dca126c377747c214e231d4c29e752612cb
Madar215 <galmadar2424@gmail.com>
2024-09-24 12:13:18 +0300

    Section Destroyer and Creator

---
+++ Assets/_Scripts/SectionDestroyer.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace _Scripts
+{
+    public class SectionDestroyer : MonoBehaviour
+    {
+        private SectionPoolManager _sectionPoolManager;
+
+        private void Awake()
+        {
+            _sectionPoolManager = GetComponentInParent<SectionPoolManager>();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.CompareTag("Section")) return;
+            
+            _sectionPoolManager.ReturnSectionToPool(other.gameObject);
+        }
+    }
+}
--- Assets/_Scripts/SectionPoolManager.cs
+++ Assets/_Scripts/SectionPoolManager.cs
@@ -8,13 +8,16 @@ namespace _Scripts
     {
         [SerializeField] private List<GameObject> sectionListPrefab = new();
         [SerializeField] private Transform sectionParent;
+        [SerializeField] private int maxActiveSections = 2;
 
         private List<GameObject> _sectionPool = new();
+        private List<GameObject> _activeSections = new();
 
         private GameObject _section;
 
         private void Awake()
         {
+            // instantiate every prefab and add it to section pool.
             foreach (var sectionPrefab in sectionListPrefab)
             {
                 _section = Instantiate(sectionPrefab, sectionParent, false);
@@ -27,10 +30,23 @@ namespace _Scripts
         {
             for (int i = 0; i < 2; i++)
             {
+                // get random section, active it, add it to active section list and position it.
                 _section = GetRandomSectionFromPool();
                 _section.SetActive(true);
+                _activeSections.Add(_section);
                 _section.transform.position = new Vector3(0f, 0f, 22f + (50f * i));
             }
+        }
+
+        private void Update()
+        {
+            if (_activeSections.Count == maxActiveSections) return;
+            
+            // get random section, active it, add it to active section list and position it.
+            _section = GetRandomSectionFromPool();
+            _section.SetActive(true);
+            _activeSections.Add(_section);
+            _section.transform.position = new Vector3(0f, 0f, 72f);
         }
 
         public GameObject GetRandomSectionFromPool()
@@ -39,13 +55,20 @@ namespace _Scripts
 
             while(true)
             {
-                randomIndex = Random.Range(0, _sectionPool.Count - 1);
+                randomIndex = Random.Range(0, _sectionPool.Count);
                 
+                // break from loop only if the random section is not active in scene.
                 if (!_sectionPool[randomIndex].activeInHierarchy)
                     break;
             }
             
             return _sectionPool[randomIndex];
+        }
+
+        public void ReturnSectionToPool(GameObject obj)
+        {
+            obj.SetActive(false);
+            _activeSections.Remove(obj);
         }
         
     }
